12572f6fe6cd41020e8efe8fca14f28bb305290b
Kaden Scott <kscottdev@gmail.com>
2021-06-23 15:29:35 -0400

    add ItemStackElement

---
+++ paper/src/main/java/dev/kscott/interfaces/core/pane/element/ItemStackElement.java
@@ -0,0 +1,47 @@
+package dev.kscott.interfaces.core.pane.element;
+
+import dev.kscott.interfaces.core.element.Element;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+/**
+ * Holds an {@link ItemStack} in an element.
+ *
+ * @see dev.kscott.interfaces.core.pane.ChestPane
+ */
+public class ItemStackElement implements Element {
+
+    /**
+     * Returns an empty {@code ItemStackElement}.
+     *
+     * @return an empty {@code ItemStackElement}.
+     */
+    public static @NonNull ItemStackElement empty() {
+        return new ItemStackElement(new ItemStack(Material.AIR));
+    }
+
+    /**
+     * The {@link ItemStack}.
+     */
+    private final @NonNull ItemStack itemStack;
+
+    /**
+     * Constructs {@code ItemStackElement}.
+     *
+     * @param itemStack the {@link ItemStack}
+     */
+    public ItemStackElement(final @NonNull ItemStack itemStack) {
+        this.itemStack = itemStack;
+    }
+
+    /**
+     * Returns this element's {@link ItemStack}.
+     *
+     * @return the {@link ItemStack}
+     */
+    public @NonNull ItemStack itemStack() {
+        return this.itemStack;
+    }
+
+}
