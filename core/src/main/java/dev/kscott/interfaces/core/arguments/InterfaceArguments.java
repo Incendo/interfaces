74eed0d78058879199e890f80b4992c3a55c923c
Kaden Scott <kscottdev@gmail.com>
2021-06-23 16:05:05 -0400

    add InterfaceArguments operator

--- core/src/main/java/dev/kscott/interfaces/core/arguments/InterfaceArguments.java
+++ core/src/main/java/dev/kscott/interfaces/core/arguments/InterfaceArguments.java
@@ -1,8 +1,27 @@
 package dev.kscott.interfaces.core.arguments;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
+
 /**
  * Holds arguments passed into an interface.
  */
 public interface InterfaceArguments {
+
+    /**
+     * Returns the value at the given key.
+     *
+     * @param key the key
+     * @param <T> the value's type
+     * @return the value
+     */
+    <T> T get(final @NonNull String key);
+
+    /**
+     * Sets a value of the argument.
+     *
+     * @param key   the key
+     * @param value the value
+     */
+    void set(final @NonNull String key, final @NonNull Object value);
 
 }
