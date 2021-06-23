523a37cc27264a67b3c6de901ebde9b78b5e85e3
Kaden Scott <kscottdev@gmail.com>
2021-06-23 14:45:06 -0400

    add gradle buildscript

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+import net.kyori.indra.IndraPlugin
+import net.kyori.indra.IndraPublishingPlugin
+import net.kyori.indra.IndraCheckstylePlugin
+import net.kyori.indra.repository.sonatypeSnapshots
+
+plugins {
+    id("net.kyori.indra")
+    id("net.kyori.indra.publishing") apply false
+    id("net.kyori.indra.checkstyle") apply false
+}
+
+group = "dev.kscott.interfaces"
+version = "1.0.0-SNAPSHOT"
+description = "A builder-style user interface library."
+
+subprojects {
+    apply<IndraPlugin>()
+    apply<IndraPublishingPlugin>()
+    apply<IndraCheckstylePlugin>()
+
+    repositories {
+        mavenCentral()
+        sonatypeSnapshots()
+    }
+
+    dependencies {
+        compileOnly(rootProject.libs.checker.qual)
+    }
+
+    indra {
+        mitLicense()
+
+        javaVersions {
+            target(16)
+        }
+//
+//        github("kadenscott", "interfaces") {
+//            ci(true)
+//            publishing(true)
+//        }
+
+        configurePublications {
+            pom {
+                developers {
+                    developer {
+                        id.set("kadenscott")
+                        email.set("kscottdev@gmail.com")
+                    }
+                }
+            }
+        }
+    }
+}
