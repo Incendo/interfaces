f767f88266e57ad6ee257e7b58b11b0397be3b66
Kaden Scott <kscottdev@gmail.com>
2021-06-24 05:21:50 -0400

    make transform a bi function

--- core/src/main/java/dev/kscott/interfaces/core/transform/Transform.java
+++ core/src/main/java/dev/kscott/interfaces/core/transform/Transform.java
@@ -4,7 +4,8 @@ import dev.kscott.interfaces.core.pane.Pane;
 import dev.kscott.interfaces.core.view.InterfaceView;
 
 import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 
-public interface Transform<T extends Pane> extends BiConsumer<T, InterfaceView> {
+public interface Transform<T extends Pane> extends BiFunction<T, InterfaceView, T> {
 
 }
