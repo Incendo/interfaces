903bf8c0d3d468ad7ff438f6f45b913af12ee4f0
Kaden Scott <kscottdev@gmail.com>
2021-07-06 13:14:39 -0400

    remove unused imports

--- paper/src/main/java/org/incendo/interfaces/paper/view/PlayerView.java
+++ paper/src/main/java/org/incendo/interfaces/paper/view/PlayerView.java
@@ -1,8 +1,6 @@
 package org.incendo.interfaces.paper.view;
 
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
-import org.checkerframework.checker.nullness.qual.NonNull;
 import org.incendo.interfaces.core.pane.Pane;
 import org.incendo.interfaces.core.view.InterfaceView;
 import org.incendo.interfaces.paper.PlayerViewer;
