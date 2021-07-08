628888349ed658f396be80a3390b05d78cd06a04
Kaden Scott <kscottdev@gmail.com>
2021-07-08 17:46:07 -0400

    add ChestClickContext

---
+++ paper/src/main/java/org/incendo/interfaces/paper/click/ChestClickContext.java
@@ -0,0 +1,68 @@
+package org.incendo.interfaces.paper.click;
+
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.incendo.interfaces.core.click.Click;
+import org.incendo.interfaces.core.click.ClickContext;
+import org.incendo.interfaces.core.view.InterfaceView;
+import org.incendo.interfaces.paper.pane.ChestPane;
+import org.incendo.interfaces.paper.view.ChestView;
+
+/**
+ * The click context of a chest.
+ */
+public class ChestClickContext implements ClickContext<ChestPane> {
+
+    private final @NonNull InventoryClickEvent event;
+    private final @NonNull ChestView view;
+    private final @NonNull Click click;
+
+    /**
+     * Constructs {@code ChestClickContext}.
+     *
+     * @param event the event, must be of {@link InventoryType#CHEST}
+     */
+    public ChestClickContext(
+            final @NonNull InventoryClickEvent event
+    ) {
+        this.event = event;
+
+        final @NonNull Inventory inventory = event.getInventory();
+
+        if (inventory.getType() != InventoryType.CHEST) {
+            throw new IllegalArgumentException("ChestClickContext requires an InventoryClickEvent with an InventoryType of " +
+                    "CHEST.");
+        }
+
+        if (!(inventory.getHolder() instanceof ChestView)) {
+            throw new IllegalArgumentException("The InventoryHolder wasn't a ChestView.");
+        }
+
+        this.view = (ChestView) inventory.getHolder();
+
+        this.click = new PaperClick(this.event);
+    }
+
+    @Override
+    public boolean cancelled() {
+        return this.event.isCancelled();
+    }
+
+    @Override
+    public void cancel(final boolean cancelled) {
+        this.event.setCancelled(cancelled);
+    }
+
+    @Override
+    public @NonNull ChestView view() {
+        return this.view;
+    }
+
+    @Override
+    public @NonNull Click click() {
+        return this.click;
+    }
+
+}
