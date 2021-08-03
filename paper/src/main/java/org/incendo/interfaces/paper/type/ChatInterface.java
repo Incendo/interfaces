8c9758dbe69469e980969d2327aec4fc13b42a38
Kaden Scott <kscottdev@gmail.com>
2021-08-03 00:11:10 -0400

    add ChatInterface::builder

--- paper/src/main/java/org/incendo/interfaces/paper/type/ChatInterface.java
+++ paper/src/main/java/org/incendo/interfaces/paper/type/ChatInterface.java
@@ -12,6 +12,8 @@ import org.incendo.interfaces.paper.PlayerViewer;
 import org.incendo.interfaces.paper.pane.ChatPane;
 import org.incendo.interfaces.paper.view.ChatView;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -29,6 +31,15 @@ public final class ChatInterface implements Interface<ChatPane, PlayerViewer> {
      */
     public ChatInterface(final @NonNull List<TransformContext<?, ChatPane, PlayerViewer>> transforms) {
         this.transforms = transforms;
+    }
+
+    /**
+     * Returns a new ChatInterface builder.
+     *
+     * @return a new builder
+     */
+    public static @NonNull Builder builder() {
+        return new Builder();
     }
 
     @Override
@@ -73,5 +84,65 @@ public final class ChatInterface implements Interface<ChatPane, PlayerViewer> {
 
         return chatView;
     }
+
+    /**
+     * A class that builds a book interface.
+     */
+    public static final class Builder implements Interface.Builder<ChatPane, PlayerViewer, ChatInterface> {
+
+        private final @NonNull List<@NonNull TransformContext<?, ChatPane, PlayerViewer>> transforms;
+
+        /**
+         * Constructs {@code Builder}.
+         */
+        public Builder() {
+            this.transforms = new ArrayList<>();
+        }
+
+        private Builder(final @NonNull List<@NonNull TransformContext<?, ChatPane, PlayerViewer>> transforms) {
+            this.transforms = Collections.unmodifiableList(transforms);
+        }
+
+        /**
+         * Adds a transformation to the interface.
+         *
+         * @param transform the transformation
+         * @return new builder instance
+         */
+        @Override
+        public @NonNull Builder addTransform(final @NonNull Transform<ChatPane, PlayerViewer> transform) {
+            return this.addTransform(InterfaceProperty.dummy(), 1, transform);
+        }
+
+        @Override
+        public <S> @NonNull Builder addTransform(
+                final @NonNull InterfaceProperty<S> property,
+                final int priority,
+                final @NonNull Transform<ChatPane, PlayerViewer> transform
+        ) {
+            final List<TransformContext<?, ChatPane, PlayerViewer>> transforms = new ArrayList<>(this.transforms);
+            transforms.add(
+                    TransformContext.of(
+                            property,
+                            priority,
+                            transform
+                    )
+            );
+
+            return new Builder(transforms);
+        }
+
+        /**
+         * Constructs and returns the interface.
+         *
+         * @return the interface
+         */
+        @Override
+        public @NonNull ChatInterface build() {
+            return new ChatInterface(this.transforms);
+        }
+
+    }
+
 
 }
