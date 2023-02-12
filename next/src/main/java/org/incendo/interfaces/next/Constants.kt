d45c8bacaafabb0c7f5615b5643ce10cbd0a6507
broccolai <me@broccol.ai>
2023-02-12 17:05:39 +0000

    fix: add a supervisor job to interfaces scope

--- next/src/main/java/org/incendo/interfaces/next/Constants.kt
+++ next/src/main/java/org/incendo/interfaces/next/Constants.kt
@@ -1,9 +1,10 @@
 package org.incendo.interfaces.next
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.newFixedThreadPoolContext
 
 internal object Constants {
 
-    internal val SCOPE = CoroutineScope(newFixedThreadPoolContext(5, "interfaces"))
+    internal val SCOPE = CoroutineScope(SupervisorJob() + newFixedThreadPoolContext(5, "interfaces"))
 }
