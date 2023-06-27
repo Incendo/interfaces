4af32d3f9bdb9718da50140e71efce2cfaca7382
Kieran Wallbanks <kieran.wallbanks@gmail.com>
2023-06-27 10:38:26 +0100

    fix: Stronger cache eviction to fix memory leaks
    Sometimes interface views can hold a reference to the player object which would prevent them from being evicted from a weak hash map, thus causing a memory leak.

--- next/src/main/java/org/incendo/interfaces/next/utilities/PlayerDataMap.kt
+++
@@ -1,7 +0,0 @@
-package org.incendo.interfaces.next.utilities
-
-import org.bukkit.entity.Player
-import org.incendo.interfaces.next.view.InterfaceView
-import java.util.WeakHashMap
-
-public open class PlayerDataMap : MutableMap<Player, InterfaceView> by WeakHashMap()
--- next/src/main/java/org/incendo/interfaces/next/view/PlayerInterfaceView.kt
+++ next/src/main/java/org/incendo/interfaces/next/view/PlayerInterfaceView.kt
@@ -3,10 +3,10 @@ package org.incendo.interfaces.next.view
 import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryType
+import org.incendo.interfaces.next.InterfacesListeners
 import org.incendo.interfaces.next.interfaces.PlayerInterface
 import org.incendo.interfaces.next.inventory.PlayerInterfacesInventory
 import org.incendo.interfaces.next.pane.PlayerPane
-import org.incendo.interfaces.next.utilities.PlayerDataMap
 
 public class PlayerInterfaceView internal constructor(
     player: Player,
@@ -17,12 +17,9 @@ public class PlayerInterfaceView internal constructor(
     // todo(josh): should player interface views hold a parent?
     null
 ) {
-    internal companion object {
-        internal val OPEN_VIEWS = PlayerDataMap()
-    }
 
     init {
-        OPEN_VIEWS[player] = this
+        InterfacesListeners.INSTANCE.openPlayerInterfaceViews.put(player.uniqueId, this)
     }
 
     override fun title(value: Component) {
